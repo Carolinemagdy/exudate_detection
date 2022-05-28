c7ae810e6dca767faea75b0f6a1e1ab8c99cd05e
Ahmed Habib <73848497+AhmedHabib00@users.noreply.github.com>
2022-05-28 16:16:13 +0200

    remove normalization from kirsch edges

--- misc/KirschEdges.py
+++ misc/KirschEdges.py
@@ -2,6 +2,7 @@ import numpy as np
 from scipy import ndimage
 import cv2
 
+
 def kirschEdges(gray):
     if gray.ndim > 2:
         raise Exception("Image should be grayscale.")
@@ -29,16 +30,22 @@ def kirschEdges(gray):
     kernelG8 = np.array([[-3, 5, 5],
                          [-3, 0, 5],
                          [-3, -3, -3]], dtype=np.float32) / 15
-    print(gray.dtype, "Data Type")
-    x = cv2.filter2D(gray, cv2.CV_64F, kernelG1)
-    g1 = cv2.normalize(cv2.filter2D(gray, cv2.CV_64F, kernelG1), None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8UC1)
-    g2 = cv2.normalize(cv2.filter2D(gray, cv2.CV_64F, kernelG2), None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8UC1)
-    g3 = cv2.normalize(cv2.filter2D(gray, cv2.CV_64F, kernelG3), None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8UC1)
-    g4 = cv2.normalize(cv2.filter2D(gray, cv2.CV_64F, kernelG4), None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8UC1)
-    g5 = cv2.normalize(cv2.filter2D(gray, cv2.CV_64F, kernelG5), None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8UC1)
-    g6 = cv2.normalize(cv2.filter2D(gray, cv2.CV_64F, kernelG6), None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8UC1)
-    g7 = cv2.normalize(cv2.filter2D(gray, cv2.CV_64F, kernelG7), None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8UC1)
-    g8 = cv2.normalize(cv2.filter2D(gray, cv2.CV_64F, kernelG8), None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8UC1)
+    #g1 = cv2.normalize(cv2.filter2D(gray, cv2.CV_64F, kernelG1), None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8UC1)
+    g1 = cv2.filter2D(gray, cv2.CV_64F, kernelG1)
+    g2 = cv2.filter2D(gray, cv2.CV_64F, kernelG2)
+    g3 = cv2.filter2D(gray, cv2.CV_64F, kernelG3)
+    g4 = cv2.filter2D(gray, cv2.CV_64F, kernelG4)
+    g5 = cv2.filter2D(gray, cv2.CV_64F, kernelG5)
+    g6 = cv2.filter2D(gray, cv2.CV_64F, kernelG6)
+    g7 = cv2.filter2D(gray, cv2.CV_64F, kernelG7)
+    g8 = cv2.filter2D(gray, cv2.CV_64F, kernelG8)
+    # g2 = cv2.normalize(cv2.filter2D(gray, cv2.CV_64F, kernelG2), None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8UC1)
+    # g3 = cv2.normalize(cv2.filter2D(gray, cv2.CV_64F, kernelG3), None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8UC1)
+    # g4 = cv2.normalize(cv2.filter2D(gray, cv2.CV_64F, kernelG4), None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8UC1)
+    # g5 = cv2.normalize(cv2.filter2D(gray, cv2.CV_64F, kernelG5), None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8UC1)
+    # g6 = cv2.normalize(cv2.filter2D(gray, cv2.CV_64F, kernelG6), None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8UC1)
+    # g7 = cv2.normalize(cv2.filter2D(gray, cv2.CV_64F, kernelG7), None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8UC1)
+    # g8 = cv2.normalize(cv2.filter2D(gray, cv2.CV_64F, kernelG8), None, 0, 255, cv2.NORM_MINMAX, cv2.CV_8UC1)
     magn = cv2.max(
         g1, cv2.max(
             g2, cv2.max(
@@ -55,5 +62,4 @@ def kirschEdges(gray):
         )
     )
     return magn
-
 
